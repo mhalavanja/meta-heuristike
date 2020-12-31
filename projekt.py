2f2459662116c3b3fb4407e66ce78479c5293a95
mhalavanja <mhalavanja@gmail.com>
2020-12-31 16:55:30 +0100

    Dodana prva verzija GA

--- projekt.py
+++ projekt.py
@@ -4,7 +4,6 @@ import numpy as np
 
 def getMatrix(n: int):
     matrix = np.random.rand(n, n)
-    # print(type(matrix))
     return matrix
 
 def get01Matrix(matrix: np.array, cut: float):
@@ -38,7 +37,6 @@ def getRandomStartingSolution(n: int):
             t = t - (sum - n)
             if t <= 0:
                 break
-        # print(sum)
         enc.append(t)
     for i in range(n):
         l.append(i)
@@ -54,12 +52,8 @@ def getFitnessOfSolution(sol: tuple, matrix: np.array):
     order, enc = sol
     fit = 0
     cur = 0
-    # print(order)
-    # print(enc)
     for l in enc:
         ntup = order[cur : cur + l]
-        # print(ntup, l)
-        # print(enc)
         ntupWorks = True
         for i in range(l):
             a = ntup[i]
@@ -70,7 +64,7 @@ def getFitnessOfSolution(sol: tuple, matrix: np.array):
                 b = ntup[i + 1]
             if matrix[a][b] == 0:
                 ntupWorks = False
-            # print(a, b, matrix[a][b])
+                break
         if ntupWorks:
             fit += l
         cur += l
@@ -91,46 +85,37 @@ def getFitnessOfDrawnSolution(sol: np.array, matrix: np.array):
                 b = ntup[i + 1]
             if matrix[a][b] == 0:
                 ntupWorks = False
-            # print(a, b, matrix[a][b])
         if ntupWorks:
             fit += l
     return fit
 
-# 1) Koliko treba biti susjedstvo?
-# 2) Susjedstvo mozemo mijenjati takoder da mijenjamo i granice mozda
-# 3) Mislim da je bolje kada mijenjamo i granice
-def getHCNeighbourhood(sol: tuple):
-    def swap(lista: np.array, i: int, j: int):
+def swap(lista: np.array, i: int, j: int):
         temp = lista[i]
         lista[i] = lista[j]
         lista[j] = temp
         return lista
 
-    def changeEnc(enc: np.array, i: int, j: int):
-        a = enc[i]
-        b = enc[j]
-        # print(i, j)
-        # print("Pocetak: ", enc, sum(enc))
-        if a != 1 and b != 3:
-            enc[i] = a - 1
-            enc[j] = b + 1
-            # print("1: ", enc, sum(enc))
-            # return enc
-        elif a != 3 and b != 1:
-            enc[i] = a + 1
-            enc[j] = b - 1
-        # print("2: ", enc, sum(enc))
-        return enc
+def changeEnc(enc: np.array, i: int, j: int):
+    a = enc[i]
+    b = enc[j]
+    if a != 1 and b != 3:
+        enc[i] = a - 1
+        enc[j] = b + 1
+    elif a != 3 and b != 1:
+        enc[i] = a + 1
+        enc[j] = b - 1
+    return enc
 
+# 1) Koliko treba biti susjedstvo?
+# 2) Susjedstvo mozemo mijenjati takoder da mijenjamo i granice mozda
+# 3) Mislim da je bolje kada mijenjamo i granice
+def getHCNeighbourhood(sol: tuple):
     order, enc = sol
     neigh = []
     swapPairs = np.random.randint(0, n, 2 * n)
     changeWhat = np.random.randint(0, 2, n)
     encToChange = np.random.randint(0, len(enc), 2 * n)
-    # print(len(swapPairs))
     for i in range(n):
-        # print(swapPairs[i], swapPairs[i + 1])
-        # print(changeWhat[i])
         if changeWhat[i] == 0 or encToChange[i] == encToChange[i + 1]:
             neigh.append((swap(order, swapPairs[i], swapPairs[i + 1]), enc))
         else:
@@ -146,11 +131,9 @@ def hillClimbing(n: int, repeat: int, matrix: np.array):
         bestFit = getFitnessOfSolution(bestSol, matrix)
         for __ in range(n*n):
             neigh = getHCNeighbourhood(bestSol)
-            # print(neigh)
             for j in range(len(neigh)):
                 curOrder = neigh[j][0]
                 curEnc = neigh[j][1]
-                # print(curEnc, curOrder)
                 curFit = getFitnessOfSolution((curOrder,curEnc), matrix)
                 if(curFit >= bestFit):
                     bestFit = curFit
@@ -166,11 +149,131 @@ def hillClimbing(n: int, repeat: int, matrix: np.array):
 #TODO: implementirati GA sljedece
 # 1) Ne znam jel se isplati napraviti HC kod odabira početne generacije za GA
 # 2) Treba odlučiti koja vrsta GA je bolja ili kako cemo tocno
-def selectionForGA(neigh: np.array, matrix: np.array):
-    pass
+def selectionForGA(generation: np.array, line: np.array, fitSum: int, k: int):
+    selected = []
+    p = fitSum/float(k)
+    [r] = np.random.uniform(0, p, 1)
+    cur = 0
+    for i in range (k):
+        curPointer = r + float(i * p)
+        while curPointer - line[cur][0] < 0:
+            cur += 1
+        selected.append(generation[cur])
+    return selected
 
-def crossingForGA():
-    pass
+def mutate(sol: tuple):
+    order, enc = sol
+    swapPairs = np.random.randint(0, n, 2)
+    [changeWhat] = np.random.randint(0, 2, 1)
+    encToChange = np.random.randint(0, len(enc), 2)
+    if changeWhat == 0 or encToChange[0] == encToChange[1]:
+        return(swap(order, swapPairs[0], swapPairs[1]), enc)
+    else:
+        return(order, changeEnc(enc, encToChange[0], encToChange[1]))
+
+# 1) Koristimo križanje poretka zato što je bitan redosljed u kojem su brojevi, a ne 
+#    njihov apsolutni poredak u listi
+def crossingForGA(n: int, k: int, population: np.array):
+    def crossing(n: int, solA: tuple, solB: tuple):
+        [pos1, pos2] = np.random.randint(0, n, 2)
+        if pos2 < pos1:
+            tmp = pos1
+            pos1 = pos2
+            pos2 = tmp
+        [orderA, encA] = solA
+        [orderB, encB] = solB
+        childAord = [None] * n 
+        childBord = [None] * n
+        setA = set()
+        setB = set()
+        for i in range(pos1, pos2):
+            childAord[i] = orderA[i]
+            setA.add(orderA[i])
+            childBord[i] = orderB[i]
+            setB.add(orderB[i])
+        j = pos2
+        for i in range(pos2, n):
+            if orderB[i] not in setA:
+                childAord[j] = orderB[i]
+                j += 1
+        for i in range(0, pos1):
+            if j == n:
+                j = 0
+            if orderB[i] not in setA:
+                childAord[j] = orderB[i]
+                j += 1
+        j = pos2
+        for i in range(pos2, n):
+            if orderA[i] not in setB:
+                childBord[j] = orderA[i]
+                j += 1
+        for i in range(0, pos1):
+            if j == n:
+                j = 0
+            if orderA[i] not in setB:
+                childBord[j] = orderA[i]
+                j += 1
+        return ((childAord, encA), (childBord, encB))
+
+    # [l, s] = np.random.randint(0, n, 2) # l -> duljina ntupa, s -> startni indeks
+    mutation = np.random.choice(2, n, p=[0.95, 0.05])
+    newGeneration = []
+    # l = l % 3 + 1
+    cur = 0
+    while cur < n:
+        for i in range(k):
+            a = population[i] #jos [0] ako se i fit prenosi s populacijom
+            b = population[(2 * i) % k]
+            c, d = crossing(n, a, b)
+            if mutation[cur] == 1:
+                c = mutate(c)
+            if cur == n:
+                return newGeneration
+            cur += 1
+            newGeneration.append(c)
+            if mutation[cur] == 1:
+                d = mutate(d)
+            cur += 1
+            newGeneration.append(d)
+            if cur == n:
+                return newGeneration
+    return newGeneration
+
+# 1) k je veličina populacije za križanje
+def geneticAlgorithm(n: int, k: int, numOfIter: int, matrix: np.array):
+    generation = []
+    fitSum = 0
+    line = []
+    bestFit = 0
+    bestSol = None
+    for __ in range(numOfIter):    
+        if len(generation) == 0:
+            for __ in range(n):
+                sol = getRandomStartingSolution(n)
+                fit = getFitnessOfSolution(sol, matrix)
+                if fit > bestFit:
+                    bestFit = fit
+                    bestSol = sol
+                newFitSum = fitSum + fit
+                line.append((fitSum, newFitSum))
+                fitSum = newFitSum
+                generation.append(sol) #mozda mozemo ovaj fit iskoristit i za kasnije
+            selected = selectionForGA(generation, line, fitSum, k)
+            generation = crossingForGA(n, k, selected)
+        else:
+            for i in range(n):
+                sol = generation[i]
+                fit = getFitnessOfSolution(sol, matrix)
+                if fit > bestFit:
+                    bestFit = fit
+                    bestSol = sol
+                newFitSum = fitSum + fit
+                fitSum = newFitSum
+                line.append((fitSum, newFitSum))
+                generation.append((sol, fit))
+            selected = selectionForGA(generation, line, fitSum, k)
+            generation = crossingForGA(n, k, selected)
+    return (bestSol, bestFit)
 
 # Ovdje upisujemo parametre
 # n -> veličina matrice
@@ -181,9 +284,10 @@ cut = 0.5
 matrix01 = get01Matrix(matrix, cut)
 
 # fitDrawn = getFitnessOfDrawnSolution(getDrawnSolution(sol), matrix01)
-# print(fitDrawn)
 
-bestSol, bestFit = hillClimbing(n, 5, matrix01)
-print(bestFit)
-print(getDrawnSolution(bestSol))
-# print(getDrawnSolution(sol))
+# bestSol, bestFit = hillClimbing(n, 5, matrix01)
+k = 10
+numOfIter = 20
+[sol, fit] = geneticAlgorithm(n, k, numOfIter, matrix01)
+print(getDrawnSolution(sol))
+print(fit)
